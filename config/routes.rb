0156d57ae0971a45bff9003fd88966cb6f16ce81
mvangg <marivang@live.com>
2023-12-04 16:56:31 -0800

    blog update

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'blogs/new' => 'blog#new', as: 'new'
   get 'blogs/:id' => 'blog#show', as: 'blog'
   post 'blogs' => 'blog#create'
+  get 'books/:id/edit' => 'blog#edit', as: 'edit_blog'
+  delete 'blogs/:id' => 'blog#destroy', as: 'delete_blog'
 
   root 'blog#index' 
 end
